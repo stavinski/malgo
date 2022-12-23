aaa0004e5ec81c23c46b2a7cd3d48e30323297ec
Michael Cromwell <stavinski_2000@yahoo.com>
2022-12-23 08:25:05 +0000

    adding wifi profile retrieval

--- wifi/main_windows.go
+++ wifi/main_windows.go
@@ -1,33 +1,89 @@
 //go:build windows
 // +build windows
 
+// Retrieve WLAN profile passwords from Windows using Win32 API calls
 package main
 
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"unsafe"
 
 	"github.com/stavinski/malgo/win32"
 )
 
+// XML marshal type for a WLAN profile
+type ProfileXML struct {
+	// TBC
+}
+
 func main() {
-	var negotiatedVer uint32
-	var hWlan uintptr
-	res, _, err := win32.ProcWlanOpenHandle.Call(uintptr(uint32(2)), win32.NULL, uintptr(unsafe.Pointer(&negotiatedVer)), uintptr(unsafe.Pointer(&hWlan)))
+	var (
+		negotiatedVer uint32
+		hWlan         uintptr
+		res           uintptr
+		err           error
+	)
+
+	res, _, err = win32.ProcWlanOpenHandle.Call(uintptr(uint32(2)),
+		win32.NULL,
+		uintptr(unsafe.Pointer(&negotiatedVer)),
+		uintptr(unsafe.Pointer(&hWlan)))
 	if res != win32.ERROR_SUCCESS {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return
 	}
-	defer win32.ProcWlanCloseHandle.Call(uintptr(hWlan), win32.NULL)
+	defer win32.ProcWlanCloseHandle.Call(hWlan, win32.NULL)
 	interfaces := &win32.WLAN_INTERFACE_INFO_LIST{}
 	res, _, err = win32.ProcWlanEnumInterfaces.Call(hWlan, win32.NULL, uintptr(unsafe.Pointer(&interfaces)))
 	if res != win32.ERROR_SUCCESS {
 		fmt.Fprintf(os.Stderr, err.Error())
 		return
 	}
+	// release interfaces memory at the end of the func
 	defer win32.ProcWlanFreeMemory.Call(uintptr(unsafe.Pointer(interfaces)))
 
-	fmt.Println("%v ifaces found.", interfaces.NumberOfItems)
+	fmt.Printf("%v ifaces found.\n", interfaces.NumberOfItems)
+
+	for i := uint32(0); i < interfaces.NumberOfItems; i++ {
+		var profList *win32.WLAN_PROFILE_INFO_LIST
+		iface := interfaces.InterfaceInfo[i]
+		res, _, err = win32.ProcWlanGetProfileList.Call(hWlan,
+			uintptr(unsafe.Pointer(&iface.InterfaceGUID)),
+			win32.NULL,
+			uintptr(unsafe.Pointer(&profList)))
+		if res != win32.ERROR_SUCCESS {
+			fmt.Fprintf(os.Stderr, err.Error())
+			return
+		}
+
+		for j := uint32(0); j < profList.NumberOfItems; j++ {
+			var profXMLPtr uintptr
+			profListEntry := profList.ProfileInfo[j]
+			fmt.Printf("Profile: %v\n", syscall.UTF16ToString(profListEntry.ProfileName[:]))
+			flags := win32.WLAN_PROFILE_GET_PLAINTEXT_KEY
+			res, _, err = win32.ProcWlanGetProfile.Call(hWlan,
+				uintptr(unsafe.Pointer(&iface.InterfaceGUID)),
+				uintptr(unsafe.Pointer(&profListEntry.ProfileName)),
+				win32.NULL,
+				uintptr(unsafe.Pointer(profXMLPtr)),
+				uintptr(unsafe.Pointer(&flags)),
+				win32.NULL)
+			if res != win32.ERROR_SUCCESS {
+				fmt.Fprintf(os.Stderr, err.Error())
+				return
+			}
+
+			profXML := win32.UTF16PtrToString((*uint16)(unsafe.Pointer(profXMLPtr)))
+			fmt.Println(profXML)
+
+			// release XML string memory
+			win32.ProcWlanFreeMemory.Call(profXMLPtr)
+		}
+
+		// release profList memory
+		win32.ProcWlanFreeMemory.Call(uintptr(unsafe.Pointer(profList)))
+	}
 
 }
--- win32/common_windows.go
+++ win32/common_windows.go
@@ -4,12 +4,19 @@
 // Common types and function calls for the Win32 API
 package win32
 
-import "syscall"
+import (
+	"syscall"
+	"unicode/utf16"
+	"unsafe"
+)
 
 const (
-	NULL                 = uintptr(0)
-	ERROR_SUCCESS        = uintptr(0)
-	WLAN_MAX_NAME_LENGTH = 256
+	NULL          = uintptr(0)
+	ERROR_SUCCESS = uintptr(0)
+
+	// WLAN values
+	WLAN_MAX_NAME_LENGTH           = 256
+	WLAN_PROFILE_GET_PLAINTEXT_KEY = 4
 )
 
 var (
@@ -20,12 +27,19 @@ var (
 	ProcWlanCloseHandle    = modWlanapi.NewProc("procWlanCloseHandle")
 	ProcWlanEnumInterfaces = modWlanapi.NewProc("WlanEnumInterfaces")
 	ProcWlanFreeMemory     = modWlanapi.NewProc("WlanFreeMemory")
+	ProcWlanGetProfileList = modWlanapi.NewProc("WlanGetProfileList")
+	ProcWlanGetProfile     = modWlanapi.NewProc("WlanGetProfile")
 )
 
 type GUID struct {
 	Data1        uint32
 	Data2, Data3 uint16
 	Data4        [8]byte
+}
+
+type WLAN_PROFILE_INFO_LIST struct {
+	NumberOfItems, Index uint32
+	ProfileInfo          [128]WLAN_PROFILE_INFO
 }
 
 type WLAN_PROFILE_INFO struct {
@@ -42,4 +56,21 @@ type WLAN_INTERFACE_INFO struct {
 type WLAN_INTERFACE_INFO_LIST struct {
 	NumberOfItems, Index uint32
 	InterfaceInfo        [64]WLAN_INTERFACE_INFO
+}
+
+func UTF16PtrToString(p *uint16) string {
+	if p == nil {
+		return ""
+	}
+	if *p == 0 {
+		return ""
+	}
+
+	// Find NUL terminator.
+	n := 0
+	for ptr := unsafe.Pointer(p); *(*uint16)(ptr) != 0; n++ {
+		ptr = unsafe.Pointer(uintptr(ptr) + unsafe.Sizeof(*p))
+	}
+
+	return string(utf16.Decode(unsafe.Slice(p, n)))
 }
